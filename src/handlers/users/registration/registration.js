b5043992cb46ef20bcedd10633c5a1564499e16d
anatol <anatoool@yandex.ru>
2019-03-10 17:25:37 +0700

    start role checker

--- src/handlers/users/registration/registration.js
+++ src/handlers/users/registration/registration.js
@@ -1,5 +1,10 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const userScheme = require('../../../database/schemes/userScheme');
+const userSendConfirmationMail = require('../../../emails/registration-confirmation/registrationConfirm');
 const userRegistrationValidate = require('./registrationValidate');
-const userRegistrationDB = require('./registration.db');
+
+const User = mongoose.model("User", userScheme);
 
 const userRegistration = async (req, res) => {
   const user = req.body;
@@ -10,7 +15,23 @@ const userRegistration = async (req, res) => {
     return res.status(422).send({...errors});
   }
 
-  userRegistrationDB({req, res, user});
+  const hashedPassword = await bcrypt.hash(user.password, 10);
+  const confirmCode =
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+  const newUser = new User({
+    name: user.name,
+    email: user.email,
+    role: 'user',
+    confirmed: false,
+    password: hashedPassword,
+    confirmCode,
+  });
+
+  await newUser.save();
+  userSendConfirmationMail({ email: user.email, confirmCode });
+  return res.status(200).send({response: 'User was created'});
 };
 
 module.exports = userRegistration;
